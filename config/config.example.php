907f74c953cb2c00e92573d4be857f04de075e79
RDMackan999 <marcus.moller@regiondalarna.se>
2026-07-02 11:53:22 +0200

    Add app config example

---
+++ config/config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'app' => [
+        'name' => 'Uthyrning',
+        'environment' => 'local',
+        'debug' => true,
+        'timezone' => 'Europe/Stockholm',
+        'base_url' => 'http://localhost',
+    ],
+];
