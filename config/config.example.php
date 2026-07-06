1f70d53e34541cf7192170ebaae3d8dcea71508b
RDMackan999 <marcus.moller@regiondalarna.se>
2026-07-06 13:08:14 +0200

    Set default backend environment

--- config/config.example.php
+++ config/config.example.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 return [
     'app' => [
         'name' => 'Uthyrning',
-        'environment' => 'local',
+        'environment' => 'development',
         'debug' => true,
         'timezone' => 'Europe/Stockholm',
         'base_url' => 'http://localhost',
+        'version' => '0.1.0',
     ],
 ];
