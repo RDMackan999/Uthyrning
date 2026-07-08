490ae5e1537846bb798438339ce36925a1925c25
RDMackan999 <marcus.moller@regiondalarna.se>
2026-07-08 10:10:21 +0200

    Align database example config keys

--- config/database.example.php
+++ config/database.example.php
@@ -7,7 +7,7 @@ return [
         'driver' => 'mysql',
         'host' => '127.0.0.1',
         'port' => 3306,
-        'name' => 'uthyrning_dev',
+        'database' => 'uthyrning_dev',
         'username' => 'root',
         'password' => '',
         'charset' => 'utf8mb4',
